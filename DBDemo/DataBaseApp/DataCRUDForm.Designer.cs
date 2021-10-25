9da2c5b51aa9e37f32b5b51641979eb89d28cbef
Shubham Gaddam <74348677+Shubham-2401@users.noreply.github.com>
2021-10-25 11:51:48 +0530

    Add files via upload

---
+++ DBDemo/DataBaseApp/DataCRUDForm.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace DataBaseApp
+{
+    partial class DataCRUDForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgData = new System.Windows.Forms.DataGridView();
+            this.lblEmpno = new System.Windows.Forms.Label();
+            this.lblEmpName = new System.Windows.Forms.Label();
+            this.lblHireDate = new System.Windows.Forms.Label();
+            this.lblSalary = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtHireDate = new System.Windows.Forms.TextBox();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.txtEmpno = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAddEmp = new System.Windows.Forms.Button();
+            this.btnEditEmp = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearchEmp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgData
+            // 
+            this.dgData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgData.Location = new System.Drawing.Point(12, 237);
+            this.dgData.Name = "dgData";
+            this.dgData.RowHeadersWidth = 62;
+            this.dgData.RowTemplate.Height = 33;
+            this.dgData.Size = new System.Drawing.Size(776, 201);
+            this.dgData.TabIndex = 0;
+            // 
+            // lblEmpno
+            // 
+            this.lblEmpno.AutoSize = true;
+            this.lblEmpno.Location = new System.Drawing.Point(64, 9);
+            this.lblEmpno.Name = "lblEmpno";
+            this.lblEmpno.Size = new System.Drawing.Size(69, 25);
+            this.lblEmpno.TabIndex = 1;
+            this.lblEmpno.Text = "Empno";
+            this.lblEmpno.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblEmpName
+            // 
+            this.lblEmpName.AutoSize = true;
+            this.lblEmpName.Location = new System.Drawing.Point(64, 52);
+            this.lblEmpName.Name = "lblEmpName";
+            this.lblEmpName.Size = new System.Drawing.Size(95, 25);
+            this.lblEmpName.TabIndex = 2;
+            this.lblEmpName.Text = "EmpName";
+            // 
+            // lblHireDate
+            // 
+            this.lblHireDate.AutoSize = true;
+            this.lblHireDate.Location = new System.Drawing.Point(64, 100);
+            this.lblHireDate.Name = "lblHireDate";
+            this.lblHireDate.Size = new System.Drawing.Size(81, 25);
+            this.lblHireDate.TabIndex = 3;
+            this.lblHireDate.Text = "HireDate";
+            // 
+            // lblSalary
+            // 
+            this.lblSalary.AutoSize = true;
+            this.lblSalary.Location = new System.Drawing.Point(64, 151);
+            this.lblSalary.Name = "lblSalary";
+            this.lblSalary.Size = new System.Drawing.Size(59, 25);
+            this.lblSalary.TabIndex = 4;
+            this.lblSalary.Text = "Salary";
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(191, 145);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(150, 31);
+            this.txtSalary.TabIndex = 5;
+            // 
+            // txtHireDate
+            // 
+            this.txtHireDate.Location = new System.Drawing.Point(191, 97);
+            this.txtHireDate.Name = "txtHireDate";
+            this.txtHireDate.Size = new System.Drawing.Size(150, 31);
+            this.txtHireDate.TabIndex = 6;
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.Location = new System.Drawing.Point(191, 52);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(150, 31);
+            this.txtEmpName.TabIndex = 7;
+            // 
+            // txtEmpno
+            // 
+            this.txtEmpno.Location = new System.Drawing.Point(191, 12);
+            this.txtEmpno.Name = "txtEmpno";
+            this.txtEmpno.Size = new System.Drawing.Size(150, 31);
+            this.txtEmpno.TabIndex = 8;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(64, 197);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 34);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAddEmp
+            // 
+            this.btnAddEmp.Location = new System.Drawing.Point(229, 197);
+            this.btnAddEmp.Name = "btnAddEmp";
+            this.btnAddEmp.Size = new System.Drawing.Size(112, 34);
+            this.btnAddEmp.TabIndex = 10;
+            this.btnAddEmp.Text = "Add Emp";
+            this.btnAddEmp.UseVisualStyleBackColor = true;
+            this.btnAddEmp.Click += new System.EventHandler(this.btnAddEmp_Click);
+            // 
+            // btnEditEmp
+            // 
+            this.btnEditEmp.Location = new System.Drawing.Point(381, 197);
+            this.btnEditEmp.Name = "btnEditEmp";
+            this.btnEditEmp.Size = new System.Drawing.Size(112, 34);
+            this.btnEditEmp.TabIndex = 11;
+            this.btnEditEmp.Text = "Edit Emp";
+            this.btnEditEmp.UseVisualStyleBackColor = true;
+            this.btnEditEmp.Click += new System.EventHandler(this.btnEditEmp_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(535, 197);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 34);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearchEmp
+            // 
+            this.btnSearchEmp.Location = new System.Drawing.Point(666, 197);
+            this.btnSearchEmp.Name = "btnSearchEmp";
+            this.btnSearchEmp.Size = new System.Drawing.Size(112, 34);
+            this.btnSearchEmp.TabIndex = 13;
+            this.btnSearchEmp.Text = "Search Emp";
+            this.btnSearchEmp.UseVisualStyleBackColor = true;
+            this.btnSearchEmp.Click += new System.EventHandler(this.btnSearchEmp_Click);
+            // 
+            // DataCRUDForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearchEmp);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEditEmp);
+            this.Controls.Add(this.btnAddEmp);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtEmpno);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.txtHireDate);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.lblSalary);
+            this.Controls.Add(this.lblHireDate);
+            this.Controls.Add(this.lblEmpName);
+            this.Controls.Add(this.lblEmpno);
+            this.Controls.Add(this.dgData);
+            this.Name = "DataCRUDForm";
+            this.Text = "DataCRUDForm";
+            this.Load += new System.EventHandler(this.DataCRUDForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgData;
+        private System.Windows.Forms.Label lblEmpno;
+        private System.Windows.Forms.Label lblEmpName;
+        private System.Windows.Forms.Label lblHireDate;
+        private System.Windows.Forms.Label lblSalary;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtHireDate;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.TextBox txtEmpno;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAddEmp;
+        private System.Windows.Forms.Button btnEditEmp;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearchEmp;
+    }
+}
---
+++ DBDemo/DataBaseApp/DataCRUDForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLibrary;
+
+namespace DataBaseApp
+{
+    public partial class DataCRUDForm : Form
+    {
+        EmpDataStore dataStore = new EmpDataStore(@"server=(localdb)\MSSQLLocalDB;database=Training;Integrated Security=True");
+        public DataCRUDForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DataCRUDForm_Load(object sender, EventArgs e)
+        {
+            dgData.DataSource = dataStore.GetEmps();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddEmp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Emp newEmp = new Emp();
+                newEmp.EmpNo = int.Parse(txtEmpno.Text);
+                newEmp.EmpName = txtEmpName.Text;
+                newEmp.HireDate = Convert.ToDateTime(txtHireDate.Text);
+                newEmp.Salary = decimal.Parse(txtSalary.Text);
+
+                int count = dataStore.AddEmp(newEmp);
+
+                if (count == 1)
+                {
+                    MessageBox.Show("Record inserted");
+                    dgData.DataSource = dataStore.GetEmps();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtEmpno.Clear();
+            txtEmpName.Clear();
+            txtHireDate.Clear();
+            txtSalary.Clear();
+        }
+
+        private void btnEditEmp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Emp empList = new Emp();
+                empList.EmpNo = int.Parse(txtEmpno.Text);
+                empList.EmpName = txtEmpName.Text;
+                empList.HireDate = Convert.ToDateTime(txtHireDate.Text);
+                empList.Salary = decimal.Parse(txtSalary.Text);
+
+
+
+                int count = dataStore.EditEmps(empList);
+
+
+
+                if (count == 1)
+                {
+                    MessageBox.Show($"Empno {txtEmpno.Text} is edited");
+                    dgData.DataSource = dataStore.GetEmps();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtEmpno.Text == "")
+                {
+                    return;
+                }
+                else
+                {
+                    int empid = int.Parse(txtEmpno.Text);
+                    int count = dataStore.delEmps(empid);
+
+
+
+                    if (count == 1)
+                    {
+                        MessageBox.Show($"{txtEmpno.Text} is deleted");
+                        dgData.DataSource = dataStore.GetEmps();
+                    }
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSearchEmp_Click(object sender, EventArgs e)
+        {
+           if (txtEmpno.Text == string.Empty)
+            {
+                return;
+            }
+           try
+            {
+                int empno = int.Parse(txtEmpno.Text);
+                Emp emp = dataStore.GetEmpByNo(empno);
+
+                if (emp != null)
+                {
+                    txtEmpName.Text = emp.EmpName;
+                    txtHireDate.Text = emp.HireDate.ToString();
+                    txtSalary.Text = emp.Salary.ToString();
+                }
+                else
+                {
+                    MessageBox.Show($"Employee by no {empno} not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    } 
+}
