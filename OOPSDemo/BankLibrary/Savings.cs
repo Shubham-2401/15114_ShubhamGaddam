c59b27d25c8b0d4c50bceb9c4007f81830129a3e
Shubham Gaddam <74348677+Shubham-2401@users.noreply.github.com>
2021-10-26 07:08:28 +0530

    Add files via upload

---
+++ OOPSDemo/BankLibrary/Savings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class Savings : Bank, IInterest
+    {
+        public Savings()
+        {
+
+        }
+
+        public Savings(string name, double amt) : base(name, amt)
+        {
+
+        }
+        public double CalculateInterest()
+        {
+            return Balance * .1;
+        }
+        public sealed override void Withdraw(double amount)
+        {
+            if ((Balance-amount)> 0)
+            {
+                //allow to withdraw
+                Balance -= amount;
+            }
+            else
+            {
+                throw new Exception("Balance not enough. Cannot Withdraw.");
+            }
+        }
+    }
+}
